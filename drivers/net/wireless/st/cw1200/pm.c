995ce35c088a4ce233e1ed1069ad4c80f2ffb86e
Bart Van Assche <bvanassche@acm.org>
2026-04-15 10:40:52 -0700

    wifi: cw1200: Enable lock context checking
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>

--- drivers/net/wireless/st/cw1200/pm.c
+++ drivers/net/wireless/st/cw1200/pm.c
@@ -155,6 +155,7 @@ int cw1200_can_suspend(struct cw1200_common *priv)
 EXPORT_SYMBOL_GPL(cw1200_can_suspend);
 
 int cw1200_wow_suspend(struct ieee80211_hw *hw, struct cfg80211_wowlan *wowlan)
+	__cond_acquires(0, ((struct cw1200_common *)hw->priv)->conf_mutex)
 {
 	struct cw1200_common *priv = hw->priv;
 	struct cw1200_pm_state *pm_state = &priv->pm_state;
--- drivers/net/wireless/st/cw1200/pm.h
+++ drivers/net/wireless/st/cw1200/pm.h
@@ -25,7 +25,8 @@ void cw1200_pm_deinit(struct cw1200_pm_state *pm);
 int cw1200_wow_suspend(struct ieee80211_hw *hw,
 		       struct cfg80211_wowlan *wowlan);
 int cw1200_can_suspend(struct cw1200_common *priv);
-int cw1200_wow_resume(struct ieee80211_hw *hw);
+int cw1200_wow_resume(struct ieee80211_hw *hw)
+	__releases(((struct cw1200_common *)hw->priv)->conf_mutex);
 void cw1200_pm_stay_awake(struct cw1200_pm_state *pm,
 			  unsigned long tmo);
 #else
